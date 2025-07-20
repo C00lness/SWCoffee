0568bb5a33cf440106717b9283fa738f08952d00
Ermek I. <Directoryourlabel@gmail.com>
2025-07-20 15:45:19 +0400

    6 комит - добавлено расстояние, осталось причесать, потестировать conpose на разных экранах, готовность 98%~

--- domain/src/main/java/com/example/domain/useCases/CurrentLocationUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.example.domain.useCases
-
-import android.app.Activity
-import com.example.domain.repository.Repository
-
-class CurrentLocationUseCase(private val repository: Repository) {
-    fun getCurrentLocations(activity: Activity) = repository.getCurrentLocation(activity)
-}
